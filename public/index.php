81b9b7890367a6e0a793031a465d1019b8197a34
RubenZx <ruben.mondom@gmail.com>
2020-03-17 19:41:15 +0100

    feat: started with the poll using twig for templates

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-phpinfo();
+require_once __DIR__.'/../vendor/autoload.php';
+$loader = new \Twig\Loader\FilesystemLoader('../src/views');
+$twig = new \Twig\Environment($loader);
+
+$form = [
+    'asignatura' => ['AS' => 0, 'PCTR' => 1],
+    'titulacion' => ['GII' => 2, 'GIA' => 3],
+    'grupo' => ['A' => 1, 'B' => 2],
+];
+echo $twig->render('index.twig', $form);
