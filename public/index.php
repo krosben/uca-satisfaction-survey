5cf15d501bec312226481a4bf85173d50d0abc8d
KROSF <rodrigosanabria22@gmail.com>
2020-04-06 12:24:46 +0200

    feat: display errors messages

--- public/index.php
+++ public/index.php
@@ -5,68 +5,28 @@ require_once '../vendor/autoload.php';
 $app = new \App\Application();
 
 $app->router->get('/', function () use ($app) {
-    $form = [
-        'subjects' => $app->db->from('subjects')->select(['id', 'name', 'id_degree'])->all(),
-        'degrees' => $app->db->from('degrees')->select(['id', 'name'])->all(),
-        'groups' => $app->db->from('groups')->select()->all(),
-        'exam' => 6,
-        'enrollment' => 6,
-        'genders' => $app->db->from('gender')->select()->all(),
-        'mentoring' => $app->db->from('mentoring')->select()->all(),
-        'interest' => $app->db->from('interest')->select()->all(),
-        'expectedgrade' => $app->db->from('expected_grade')->select()->all(),
-        'dificulty' => $app->db->from('dificulty')->select()->all(),
-        'assistance' => $app->db->from('assistance')->select()->all(),
-        'questions' => array_group_by($app->db->from('questions')->select()->all(), 'type', 'subtype'),
-        'proffesors' => $app->db->from('proffesors')->select()->all(),
-        'answers' => ['NS', '1', '2', '3', '4', '5'],
-        'columns' => 3,
-    ];
-
-    return $app->twig->render('index.twig', $form);
+    return $app->twig->render('index.twig', $app->getFormData());
 });
 
-$app->router->get('admin', function () use ($app) {
-    return $app->twig->render('admin.twig');
-});
-
-$app->router->post('submit', function () use ($app) {
-    $validation = $app->validator->make($_POST, [
-        'degree' => 'required|numeric',
-        'subject' => 'required|numeric',
-        'group' => 'required|numeric',
-        'age' => 'required|numeric',
-        'gender' => 'required|numeric',
-        'lowest-course' => 'required|numeric',
-        'highest-course' => 'required|numeric',
-        'enrollment' => 'required|numeric',
-        'exam' => 'required|numeric',
-        'interest' => 'required|numeric',
-        'mentoring' => 'required|numeric',
-        'dificulty' => 'required|numeric',
-        'expectedgrade' => 'required|numeric',
-        'assistance' => 'required|numeric',
-        'proffesors' => 'required|array|min:1|max:3',
-        'proffesors.*' => 'required|numeric',
-        'answers' => 'required|array|equal:proffesors',
-        'answers.*' => 'required|array|min:23|max:23',
-    ]);
+$app->router->post('/', function () use ($app) {
+    $validation = $app->validator->make($_POST, $app->getFormValidationRules());
 
     $validation->validate();
 
     if ($validation->fails()) {
-        // TODO: Send errors to index
-        $errors = $validation->errors();
-        echo '<pre>';
-        print_r($errors->firstOfAll());
-        print_r($_POST);
-        echo '</pre>';
-        exit;
+        $context = $app->getFormData();
+        $context['errors'] = $validation->errors()->firstOfAll();
+
+        return $app->twig->render('index.twig', $context);
     }
 
     $app->savePoll($validation->getValidData());
 
     return $app->twig->render('success.twig');
+});
+
+$app->router->get('admin', function () use ($app) {
+    return $app->twig->render('admin.twig');
 });
 
 $app->run();
--- src/Application.php
+++ src/Application.php
@@ -6,6 +6,8 @@ use App\Rules\EqualSizeRule;
 use Opis\Database\Connection;
 use Opis\Database\Database;
 use Phroute\Phroute\Dispatcher;
+use Phroute\Phroute\Exception\HttpMethodNotAllowedException;
+use Phroute\Phroute\Exception\HttpRouteNotFoundException;
 use Phroute\Phroute\RouteCollector;
 use Rakit\Validation\Validator;
 
@@ -46,7 +48,7 @@ class Application
         $loader = new \Twig\Loader\FilesystemLoader('../src/views');
         $this->twig = new \Twig\Environment($loader);
         $this->validator = new Validator();
-        $this->validator->addValidator('equal', new EqualSizeRule());
+        $this->validator->addValidator('equal_size', new EqualSizeRule());
     }
 
     public function run()
@@ -55,11 +57,11 @@ class Application
 
         try {
             $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-        } catch (\Phroute\Phroute\Exception\HttpRouteNotFoundException $ex) {
+        } catch (HttpRouteNotFoundException $ex) {
             error_log($ex);
             http_response_code(404);
             $response = $this->twig->render('errors.twig', ['code' => 404, 'message' => 'Pagina No Encontrada']);
-        } catch (\Phroute\Phroute\Exception\HttpMethodNotAllowedException $ex) {
+        } catch (HttpMethodNotAllowedException $ex) {
             error_log($ex);
             http_response_code(405);
             $response = $this->twig->render('errors.twig', ['code' => 405, 'message' => 'Método no permitido']);
@@ -72,51 +74,93 @@ class Application
     {
         $student = [
             'age' => $data['age'],
-            'highest_course' => $data['highest-course'],
-            'lowest_course' => $data['lowest-course'],
-            'enrollment' => $data['enrollment'],
             'exam' => $data['exam'],
             'id_gender' => $data['gender'],
-            'id_mentoring' => $data['mentoring'],
             'id_interest' => $data['interest'],
+            'enrollment' => $data['enrollment'],
             'id_dificulty' => $data['dificulty'],
-            'id_expected_grade' => $data['expectedgrade'],
+            'id_mentoring' => $data['mentoring'],
             'id_assitance' => $data['assistance'],
+            'lowest_course' => $data['lowest-course'],
+            'highest_course' => $data['highest-course'],
+            'id_expected_grade' => $data['expectedgrade'],
         ];
 
         if ($this->db->insert($student)->into('students')) {
-            $id_student = $this->db->from('students')->count();
+            $id_student = $this->db->getConnection()->getPDO()->lastInsertId();
 
             $this->db->insert([
-                'id_subject' => $data['subject'],
                 'id_student' => $id_student,
                 'id_group' => $data['group'],
+                'id_subject' => $data['subject'],
             ])->into('polls');
 
             foreach ($data['answers'] as $proffesor => $answers) {
                 $id_proffesor = $data['proffesors'][$proffesor];
                 foreach ($answers as $id_question => $value) {
                     $votes = $this->db->update('answers')
-                        ->where('id_question')
-                        ->is($id_question)
-                        ->andWhere('id_proffesor')
-                        ->is($id_proffesor)
-                        ->andWhere('value')
-                        ->is($value)
+                        ->where('id_question')->is($id_question)
+                        ->andWhere('id_proffesor')->is($id_proffesor)
+                        ->andWhere('value')->is($value)
                         ->increment('votes')
                     ;
                     if (0 === $votes) {
                         $this->db->insert([
-                            'value' => $value,
                             'votes' => 1,
+                            'value' => $value,
+                            'id_students' => $id_student,
                             'id_question' => $id_question,
                             'id_proffesor' => $id_proffesor,
-                            'id_students' => $id_student,
                         ])->into('answers');
                     }
                 }
             }
         }
+    }
+
+    public function getFormData(): array
+    {
+        return [
+            'subjects' => $this->db->from('subjects')->select(['id', 'name', 'id_degree'])->all(),
+            'degrees' => $this->db->from('degrees')->select(['id', 'name'])->all(),
+            'groups' => $this->db->from('groups')->select()->all(),
+            'exam' => 6,
+            'enrollment' => 6,
+            'genders' => $this->db->from('gender')->select()->all(),
+            'mentoring' => $this->db->from('mentoring')->select()->all(),
+            'interest' => $this->db->from('interest')->select()->all(),
+            'expectedgrade' => $this->db->from('expected_grade')->select()->all(),
+            'dificulty' => $this->db->from('dificulty')->select()->all(),
+            'assistance' => $this->db->from('assistance')->select()->all(),
+            'questions' => array_group_by($this->db->from('questions')->select()->all(), 'type', 'subtype'),
+            'proffesors' => $this->db->from('proffesors')->select()->all(),
+            'answers' => ['NS', '1', '2', '3', '4', '5'],
+            'columns' => 3,
+        ];
+    }
+
+    public function getFormValidationRules(): array
+    {
+        return [
+            'degree' => 'required|numeric',
+            'subject' => 'required|numeric',
+            'group' => 'required|numeric',
+            'age' => 'required|numeric',
+            'gender' => 'required|numeric',
+            'lowest-course' => 'required|numeric',
+            'highest-course' => 'required|numeric',
+            'enrollment' => 'required|numeric',
+            'exam' => 'required|numeric',
+            'interest' => 'required|numeric',
+            'mentoring' => 'required|numeric',
+            'dificulty' => 'required|numeric',
+            'expectedgrade' => 'required|numeric',
+            'assistance' => 'required|numeric',
+            'proffesors' => 'required|array|min:1|max:3',
+            'proffesors.*' => 'required|numeric',
+            'answers' => 'required|array|equal_size:proffesors',
+            'answers.*' => 'required|array|min:23|max:23',
+        ];
     }
 
     public function redirect(string $to)
